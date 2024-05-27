c934056f520283577c9b81b1197a3f3ff844abc9
AndresV27 <andres13lda@gmail.com>
2024-05-27 12:03:03 -0600

    cambios

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
     class DatabasesConexion {
 
         private $host = "srv863.hstgr.io";
-        private $pass = 'zD~>EilCGd1';
+        private $pass = '#5?Sf1p0Vh';
         private $usua = "u484426513_multimedios022";
         private $database =  "u484426513_multimedios022";
 
@@ -19,7 +19,6 @@
 
                 $this->conectors = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->usua, $this->pass);
                 $this->conectors->exec("set names utf8");
-                //es para indicarle al sistema que los caracteres son de 8 bits
 
             }catch(PDOException $e){
                     echo "Error: " . $e->getMessage();
